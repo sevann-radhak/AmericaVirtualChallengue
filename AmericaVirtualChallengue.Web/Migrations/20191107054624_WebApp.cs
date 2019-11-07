a8fe5487de360607ef5a86c15ac1c1b68b52ab30
Sevann Radhak <sevann.radhak@gmail.com>
2019-11-07 02:50:17 -0300

    i#14 migration db

---
+++ AmericaVirtualChallengue.Web/Migrations/20191107054624_WebApp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AmericaVirtualChallengue.Web.Migrations
+{
+    public partial class WebApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
